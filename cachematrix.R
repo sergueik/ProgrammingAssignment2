55a4ca8aad6fc3b75905c96279b52510c877010e
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-06-16 21:32:03 -0400

    pretty print the code

--- cachematrix.R
+++ cachematrix.R
@@ -1,42 +1,46 @@
+## function for object with internal data 
+  
+makeCacheMatrix <- function(self = matrix()) {
+	data <- NULL
+	list(set =  function(input) {
+			self <<- input
+			data <<- NULL
+		},
+	get = function()  self, 
+	set_data = function(value) {
+			data <<- value
+		}, 
+	get_data = function(){ 
+			data 
+		} 
+	) 
+}
+
+
+## perform lengthy calculation and store the result in the sut
+
+cacheSolve <- function(x, ...) {
+	result <- x$get_data()
+	if(!is.null(result)) {
+		message('Getting cached data')
+		return(result)
+	} else {
+		message('Solving the matrix')
+	}
+	data <- x$get()
+	result <- solve(data, ...)
+	x$set_data(result)
+	result
+}
+
 ## Testing: 
 ## B = matrix(runif(16),4)
 ## O = makeCacheMatrix(B)
-## O$set_result(NULL)
+## O$set_data(NULL)
 ## D = cacheSolve(O)
 ## C = D %*% B
 ## check <- 1
-## for (cnt in 1:length(C[1,])) {check <- check * C[cnt,cnt] }
-## check <- round(check, digits = 5)
-## if (check != 1) { warning('Bad inverse')}
-## if (is.null(O$get_result())){ warning('Bad cache usage')}
+## for (cnt in 1:nrow(C)) {check <- check * C[cnt,cnt] }
+## if (round(check, digits = 5) != 1) { warning('Bad inverse')}
+## if (is.null(O$get_data())){ warning('Bad cache usage')}
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(simple_matrix = matrix()) {
-cached_result <- NULL
-set <- function(input) {
-	simple_matrix <<- input
-	cached_result <<- NULL
-}
-get <- function() simple_matrix
-set_result <- function(result) cached_result <<- result
-get_result <- function() cached_result
-list(set = set, get = get, set_result = set_result, get_result = get_result) 
-}
-
-
-## 
-
-cacheSolve <- function(x, ...) {
-m <- x$get_result()
-if(!is.null(m)) {
-	message('Getting cached data')
-	return(m)
-} else {
-	message('Solving the matrix')
-}
-data <- x$get()
-m <- solve(data, ...)
-x$set_result(m)
-m
-}

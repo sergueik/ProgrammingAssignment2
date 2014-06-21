843a4c2f6f800a0ea1427a754923343747eeea2b
Serguei Kouzmine <kouzmine_serguei@yahoo.com>
2014-06-21 19:20:45 -0400

    provided more detailed explanatory comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,16 @@
+## Testing: 
+## B = matrix(runif(16),4)
+## O = makeCacheMatrix(B)
+## O$set_data(NULL)
+## D = cacheSolve(O)
+## C = D %*% B
+## check <- 1
+## for (cnt in 1:nrow(C)) {check <- check * C[cnt,cnt] }
+## if (round(check, digits = 5) != 1) { warning('Bad inverse')}
+## if (is.null(O$get_data())){ warning('Bad cache usage')}
+
 ## function for object with internal data 
-  
+## constructs functions to deal with the data object stored within
 makeCacheMatrix <- function(self = matrix()) {
 	data <- NULL
 	list(set =  function(input) {
@@ -18,7 +29,9 @@ makeCacheMatrix <- function(self = matrix()) {
 
 
 ## perform lengthy calculation and store the result in the sut
-
+## by first trying to get result from the cache
+## and if not available compute the result directly 
+## and place it into the object's cache
 cacheSolve <- function(x, ...) {
 	result <- x$get_data()
 	if(!is.null(result)) {
@@ -32,15 +45,4 @@ cacheSolve <- function(x, ...) {
 	x$set_data(result)
 	result
 }
-
-## Testing: 
-## B = matrix(runif(16),4)
-## O = makeCacheMatrix(B)
-## O$set_data(NULL)
-## D = cacheSolve(O)
-## C = D %*% B
-## check <- 1
-## for (cnt in 1:nrow(C)) {check <- check * C[cnt,cnt] }
-## if (round(check, digits = 5) != 1) { warning('Bad inverse')}
-## if (is.null(O$get_data())){ warning('Bad cache usage')}
 
